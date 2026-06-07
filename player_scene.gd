51a4ed10182268ba6db6482d38f2e1b049654528
Simon <simonrmurcia@gmail.com>
2026-06-07 14:55:12 -0300

    Initial commit

---
+++ player_scene.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+# A dictionary that maps input map actions to direction vectors
+const inputs = {
+	"move_right": Vector2.RIGHT,
+	"move_left": Vector2.LEFT,
+	"move_down": Vector2.DOWN,
+	"move_up": Vector2.UP
+}
+
+# Stores the grid size, which is 16 (same as one tile)
+var grid_size = 16
+
+# Reference to the RayCast2D node
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+
+# Calls the move function with the appropriate input key
+# if any input map action is triggered
+func _unhandled_input(event):
+	for action in inputs.keys():
+		if event.is_action_pressed(action):
+			move(action)
+
+# Updates the direction of the RayCast2D according to the input key
+# and moves one grid if no collision is detected
+func move(action):
+	var destination = inputs[action] * grid_size
+	ray_cast_2d.target_position = destination
+	ray_cast_2d.force_raycast_update()
+	if not ray_cast_2d.is_colliding():
+		position += destination
